590e4d0ce3670aae6b852ac77688ac0b699fc46c
cscheffauer <scheffauerc@gmail.com>
2020-07-03 20:07:20 +0200

    added errorboundary to websocket connection

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,47 @@
-import React, { useEffect } from 'react';
+import React, { useState } from 'react';
 import { w3cwebsocket as W3CWebSocket } from "websocket";
+
+import './App.css';
+import Homepage from './containers/Homepage.component';
+
 
 const client = new W3CWebSocket('ws://127.0.0.1:8001');
 
-const App = () => {
 
-  useEffect(() => {
-    client.onopen = () => {
-      console.log('WebSocket Client Connected');
-    };
-    client.onmessage = (message) => {
-      console.log(message);
-    };
-  })
+const App = () => {
+  const [connection, setconnection] = useState('loading');
+
+  client.onopen = () => {
+    setconnection('established');
+    console.log('WebSocket Client Connected');
+  };
+
+  client.onerror = () => {
+    setconnection('error');
+    console.log("Can't connect to Websocket Server.")
+  }
+
+  const renderConnectionSwitch = () => {
+    switch (connection) {
+      case 'loading':
+        return <p>Loading...</p>
+      case 'established':
+        return <Homepage client={client} />
+      case 'error':
+        return <div className="errorboundary">< h3 >The Pexip Chat is currently unavailable!</h3 > <p>Pls contact support.</p></div >
+      default:
+        return <div />
+    }
+  }
 
   return (
     <div>
-      App
-    </div>
+      {
+        renderConnectionSwitch()
+      }
+    </div >
   );
+
 
 }
 
---
+++ src/containers/Homepage.component.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+type Props = {
+    client: any
+}
+
+const Homepage = ({ client }: Props) => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Homepage
