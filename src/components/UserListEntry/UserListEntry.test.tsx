a4485d39cb3640be89a995c7d5e90c3921ba7abd
Christoph Scheffauer <scheffauerc@gmail.com>
2020-07-24 00:20:29 +0200

    Edited the UserListEntry tests

--- src/components/UserListEntry/UserListEntry.test.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import UserListEntry from './UserListEntry.component';
-
-it('expect to render UserListEntry component', () => {
-    const userListEntryWrapper = shallow(<UserListEntry key={'1'} username={'Max Mustermann'} />);
-    expect(userListEntryWrapper).toMatchSnapshot();
-})
