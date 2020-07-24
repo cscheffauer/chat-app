48e1fc5cb2a7eedf4d3d82c4858924657b1e5e0c
Christoph Scheffauer <scheffauerc@gmail.com>
2020-07-24 10:26:27 +0200

    Edited ChatContainer and Homepage tests

---
+++ src/containers/ChatContainer/ChatContainer.test.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import ChatContainer from './ChatContainer.component';
+
+describe('<ChatContainer />', () => {
+    let wrapper;
+
+    const clientMock = jest.fn();
+
+    beforeEach(() => {
+        wrapper = mount(<ChatContainer client={clientMock} username={'John Doe'} />)
+    });
+
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
+    it('expect to render ChatContainer component', () => {
+        expect(wrapper).toMatchSnapshot();
+    })
+});
---
+++ src/containers/Homepage/Homepage.test.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import Homepage from './Homepage.component';
+
+describe('<Homepage />', () => {
+    let wrapper;
+
+    const clientMock = jest.fn();
+
+    beforeEach(() => {
+        wrapper = mount(<Homepage client={clientMock} />)
+    });
+
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
+    it('expect to render Homepage component', () => {
+        expect(wrapper).toMatchSnapshot();
+    })
+});
